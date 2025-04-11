dec41330a49211d06be9f4a78ce70b447c6b854d
sabrina-2aa1 <2721553@collegelacite.ca>
2025-04-11 15:32:52 -0400

    https

---
+++ prisma/migrations/20250411054032_user/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "nom" TEXT NOT NULL,
+    "type" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
